05fa12d30321acf973f5772af6169e5b38741fef
Oleg Kharitonov <o_kharitonov@mail.ru>
2024-06-13 23:35:05 +0300

    gendiff.py refactoring

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -3,31 +3,48 @@
 import json
 
 
-def get_output(file1_dict, file2_dict):
-    keys1 = list(file1_dict.keys())
-    keys2 = list(file2_dict.keys())
-    sorted_items = sorted(set(keys1 + keys2))
+def get_diff_dict(dict_1, dict_2):
+    diff = {}
+    items1, items2 = dict_1.keys(), dict_2.keys()
+    # if you need sort your result in abc-order - activate next step.
+    items = sorted(set(items1) | set(items2))
+    for item in items:
+        if item in items1 and item not in items2:
+            diff[item] = {'status': 'removed', 'value': dict_1[item]}
+        if item in items1 and item in items2 and dict_1[item] == dict_2[item]:
+            diff[item] = {'status': 'unchanged', 'value': dict_1[item]}
+        if item in items1 and item in items2 and dict_1[item] != dict_2[item]:
+            diff[item] = {'status': 'changed', 'old_value': dict_1[item],
+                          'new_value': dict_2[item]}
+        if item not in items1 and item in items2:
+            diff[item] = {'status': 'added', 'value': dict_2[item]}
+
+    return diff
+
+
+def get_output(diff):
     output = "{\n"
-    for item in sorted_items:
-        if file1_dict.get(item) and \
-                file2_dict.get(item) and \
-                file1_dict[item] == file2_dict[item]:
-            output += (f'  {item}: {str(file1_dict[item])}\n')
+    for k, v in diff.items():
+        if v['status'] == 'changed':
+            old_value = str(v['old_value'])
+            new_value = str(v['new_value'])
+            output += (f'- {k}: {old_value}\n')
+            output += (f'+ {k}: {new_value}\n')
         else:
-            if file1_dict.get(item):
-                output += (f'- {item}: {str(file1_dict[item])}\n')
-            if file2_dict.get(item):
-                output += (f'+ {item}: {str(file2_dict[item])}\n')
+            status = v['status'].replace('unchanged', ' ')\
+                                .replace('added', '+').replace('removed', '-')
+            value = str(v['value'])
+            output += (f'{status} {k}: {value}\n')
     output += '}'
+
+    output = output.replace('False', 'false').replace('True', 'true')
+
     return output
 
 
 def generate_diff(file_path1, file_path2):
-    file1_dict = json.load(open(file_path1))
-    file2_dict = json.load(open(file_path2))
-    for d in (file1_dict, file2_dict):
-        for element in d:
-            d[element] = str(
-                d[element]).replace('False', 'false').replace('True', 'true')
-    result = get_output(file1_dict, file2_dict)
+    dict_1 = json.load(open(file_path1))
+    dict_2 = json.load(open(file_path2))
+    diff_dict = get_diff_dict(dict_1, dict_2)
+    result = get_output(diff_dict)
     return result
